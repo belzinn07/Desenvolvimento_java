a3a72d45a683d741612326ed1e86c2e658ebab5f
Belmiro Neto <netobelmiro1@gmail.com>
2025-09-04 14:58:27 -0300

    v4

--- Paradigmas/src/Collections.java
+++ Paradigmas/src/Collections.java
@@ -7,30 +7,38 @@ import java.util.List; //importa a interface List
 public class Collections {
  
     public static void main(String[] args) {
-        List <Integer> notas = new ArrayList<>(Arrays.asList(5,6,7,8,9,10)); //inicia a lista de notas 
+        List <Integer> notas = new ArrayList<>(Arrays.asList(5,6,7,8,9,10,4)); //inicia a lista de notas 
 
           //Apliacando filtro dos números maiores que 5  
         List <Integer> maioresQue5 = filtrar(notas, Collections::maiorQue5);  
         
-       
+        System.out.println("As notas maiores que 5 são: "+maioresQue5);
 
-        System.out.println("As notas maiores que 5 são: " + maioresQue5);//exibe os maiores que 5
         System.out.printf("Média das notas maiores que 5 : " + calcularMedia(maioresQue5));
+  
 
 
     }
 
     //criação da do método filtrar que filtra uma lista de inteiros  
     public static List<Integer> filtrar(List<Integer> lista,Filtro filtros) {
-      List <Integer>  resultado = new ArrayList<>(); //inicia a lista resultado    
+      List <Integer>  resultado = new ArrayList<>(); //inicia a lista resultado  
+      List<Integer>  notasbaixas = new ArrayList<>();  
 
       for(int n : lista){//itera sobre cada inteiro
       
         if (filtros.testar(n)) {
           resultado.add(n);
           
+        }else{
+          notasbaixas.add(n);
+
+         
         }
       }
+
+       System.out.println("Notas baixas: " + notasbaixas);
+
       return resultado;
 
       
@@ -46,6 +54,8 @@ public class Collections {
     public static boolean maiorQue5(int notas){
       return notas>5;
 
+      
+
     }
 
    //método de calcular média
@@ -59,10 +69,5 @@ public class Collections {
 
     }
 
-  
-
-
-    
-
-
+ 
 }
---
+++ Paradigmas/src/Funcional.java
@@ -0,0 +1,39 @@
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.Arrays;
+
+
+
+public class Funcional {
+
+    public static void main(String[] args) {
+        List<Integer> notas = Arrays.asList(4,5,6,7,8,9,10);
+
+        Predicate<Integer>  maioresQue5 = nota -> nota>5;
+
+        List<Integer> maiorQue5 = notas.stream()
+        .filter(n->n>5)
+        .toList();
+        System.out.println("As notas maiores que 5 são: " + maiorQue5);//exibe os maiores que 5
+       
+        double media = notas.stream()
+        .filter(maioresQue5::test)
+        .mapToInt(n->n)
+        .average()
+        .orElse(0.0);
+
+         System.out.printf("Média das notas maiores que 5 : " + media);
+
+         List<Integer> notasBaixas = notas.stream()
+         .filter(n-> n<=5)
+         .toList();
+          System.out.println(" As notas baixas são: " + notasBaixas);
+
+
+
+
+        
+    }
+
+
+}
